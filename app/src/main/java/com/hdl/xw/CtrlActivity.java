9c0b6707a4837f82ee75f4a329760a193010c0e0
TommyDai <hdl-djl@hdlchina.com.cn>
2017-09-01 14:55:26 +0800

    Upgrade Push mechanism and Fix the Logic mode may not  response

--- app/src/main/java/com/hdl/xw/CtrlActivity.java
+++ app/src/main/java/com/hdl/xw/CtrlActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.hdl.libr.hdl_lib.Appliances.AirCondition.Parser.AirCtrlParser;
 import com.hdl.libr.hdl_lib.Appliances.Curtain.Parser.CurtainCtrlParser;
 import com.hdl.libr.hdl_lib.CommandData;
@@ -16,10 +15,11 @@ import com.hdl.libr.hdl_lib.DeviceManager.EventBusEvent.AirFeedBackEvent;
 import com.hdl.libr.hdl_lib.DeviceManager.EventBusEvent.CurtainFeedBackEvent;
 import com.hdl.libr.hdl_lib.DeviceManager.EventBusEvent.DeviceStateEvent;
 import com.hdl.libr.hdl_lib.DeviceManager.EventBusEvent.LightFeedBackEvent;
-
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+
 
 
 public class CtrlActivity extends AppCompatActivity {
@@ -27,7 +27,7 @@ public class CtrlActivity extends AppCompatActivity {
     private Button lightBtn,curtainBtn,curtainBtn2,curtainBtn3,curtainBtn4,curtainBtn5,logicBtn,airBtn;
     private AppliancesInfo appliancesInfo;
     private int lightState;
-    private int curtainState;
+
     private int airState;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +43,7 @@ public class CtrlActivity extends AppCompatActivity {
         airBtn = (Button) findViewById(R.id.airbtn);
 
         lightState = 100;//初始化灯光亮度100
-        curtainState = CurtainCtrlParser.curtainOn;//初始化窗帘开
+
         airState = AirCtrlParser.airOff;
 
         appliancesInfo = (AppliancesInfo) getIntent().getSerializableExtra("light");
@@ -62,7 +62,7 @@ public class CtrlActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //窗帘模块第二个参数 为CurtainCtrlParser.curtainOn，CurtainCtrlParser.curtainOff，CurtainCtrlParser.curtainPause其中一个
-                CommandData.curtainCtrl(CtrlActivity.this,appliancesInfo, curtainState);
+                CommandData.curtainCtrl(CtrlActivity.this,appliancesInfo, CurtainCtrlParser.curtainOn);
             }
         });
 
@@ -126,10 +126,27 @@ public class CtrlActivity extends AppCompatActivity {
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onLightFeedBackInfoEventMain(LightFeedBackEvent event){
-        lightState = event.getLightCtrlBackInfo().getBrightness()==100? 0:100;//如果返回100重置状态为0，反之重置状态100
-        Toast.makeText(this,"当前亮度 = "+event.getLightCtrlBackInfo().getBrightness(),Toast.LENGTH_SHORT).show();
-        Log.i("ctrlLight",event.getLightCtrlBackInfo().toString());
-        lightBtn.setText("当前亮度 = "+event.getLightCtrlBackInfo().getBrightness());
+        /**
+         * 控制回馈与推送都是通过此方法返回。
+         */
+//        此处代码不能识别哪个灯光返回
+        int brightness = event.getLightCtrlBackInfo().getBrightness();
+        lightState = brightness==100? 0:100;//如果返回100重置状态为0，反之重置状态100
+        lightBtn.setText("当前亮度 = "+brightness);
+
+        /*以下为灯光推送示例代码，可以识别哪个继电器，哪个调光灯，哪个回路，也可用作控制回馈。
+        按需求调用*/
+        String remarks = event.getLightCtrlBackInfo().getRemarks();//获取返回的灯光备注。如果每个灯光回路备注都唯一，可以直接通过备注判断
+        String parentRemarks = event.getLightCtrlBackInfo().getParentRemarks();//获取继电器或调光灯备注。这里可以知道是哪个设备返回的
+        int num = event.getLightCtrlBackInfo().getChannelNum();//获取回路号。这里可以获取到这个继电器或调光灯的回路号
+        Toast.makeText(this,parentRemarks+" 的 "+remarks+" 回路号："+num+" 返回"+" 亮度为："+brightness,Toast.LENGTH_SHORT).show();
+
+        /**
+         * 如果备注不能满足需求，则可通过子网id和设备id查找。子网id，设备id共同确定唯一设备。
+         */
+
+
+
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -138,11 +155,17 @@ public class CtrlActivity extends AppCompatActivity {
         //窗帘模块：curState:0=停止,1=打开,2=关闭。
         //开合帘电机，卷帘电机：curState:1-100开合度。也会返回0，1，2的状态
         //建议开合帘电机，卷帘电机按停止后再读取当前状态来获取当前状态值
-        Toast.makeText(this,"当前窗户状态"+curState,Toast.LENGTH_SHORT).show();
+
+        String remarks = event.getCurtainCtrlBackInfo().getRemarks();
+        String parentRemarks = event.getCurtainCtrlBackInfo().getParentRemarks();
+        int num = event.getCurtainCtrlBackInfo().getNum();
+        Toast.makeText(this,parentRemarks+" 的 "+remarks+" 回路号："+num+" 返回"+" 状态为："+curState,Toast.LENGTH_SHORT).show();
+
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onAirFeedBackInfoEventMain(AirFeedBackEvent event){
+//        空调面板一般只有一个
         airState = event.getAirCtrlBackInfo().getIsOn()==1?0:1;
         if(event.getAirCtrlBackInfo().getIsOn()==0){
             Toast.makeText(this,"空调关",Toast.LENGTH_SHORT).show();
