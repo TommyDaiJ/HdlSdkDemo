714e796fc204bdd154712b107c5767fed88eb897
TommyDai <hdl-djl@hdlchina.com.cn>
2018-12-18 11:22:04 +0800

    v1.5.8.20181218_beta 修复搜索过程中有可能导致崩溃的bug

--- app/src/main/java/com/hdl/xw/MainActivity.java
+++ app/src/main/java/com/hdl/xw/MainActivity.java
@@ -40,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog proDia;
 
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
