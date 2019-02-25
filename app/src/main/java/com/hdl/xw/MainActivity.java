23516f95ec44d852e82106abd4a305e164cada70
TommyDai <hdl-djl@hdlchina.com.cn>
2019-02-25 11:45:58 +0800

    v1.5.8.20190225_beta 修复部分已知bug

--- app/src/main/java/com/hdl/xw/MainActivity.java
+++ app/src/main/java/com/hdl/xw/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         HDLCommand.init(this);
         if (!EventBus.getDefault().isRegistered(this)) {
             EventBus.getDefault().register(this);
